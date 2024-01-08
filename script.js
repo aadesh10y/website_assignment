37c1b8f696d965d002bd0b4d2c081a0b7c12d057
aadesh10y <138278725+aadesh10y@users.noreply.github.com>
2024-01-08 19:29:06 +0545

    adv

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
+let searchBtn = document.querySelector("#search-btn");
+let searchBar = document.querySelector(".search-bar-container");
+let formBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector(".login-form-container");
+let formClose = document.querySelector("#form-close");
 
-window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active'); 
-}
+window.onscroll = () => {
+  searchBtn.classList.remove("fa-times");
+  searchBar.classList.remove("active");
+};
 
-searchBtn.addEventListener('click', () => {
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');   
+searchBtn.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
+
+  // Toggle the search and cancel icons using Font Awesome classes
+  if (searchBar.classList.contains("active")) {
+    searchBtn.innerHTML = '<i class="fas fa-times"></i>';
+  } else {
+    searchBtn.innerHTML = '<i class="fas fa-search"></i>';
+  }
+});
+
+formBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
+});
+
+formClose.addEventListener("click", () => {
+  loginForm.classList.remove("active");
 });
