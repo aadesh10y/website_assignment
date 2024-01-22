2bae05a7c7e288d7e51443c86dd24fcd6d19334b
aadesh10y <138278725+aadesh10y@users.noreply.github.com>
2024-01-22 23:42:49 +0545

    news section continue

--- script.js
+++ script.js
@@ -48,3 +48,52 @@ videobtn.forEach(btn =>{
     document.querySelector('#video-slider').src = src;
   });
 });
+
+const API_KEY = "ac693a0525364bcfba8eec98622884d3";
+const url = "https://newsapi.org/v2/everything?q=";
+window.addEventListener('load', () => fetchFootballNews("Europe"));
+
+async function fetchFootballNews() {
+  const query = "football";
+  const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+  const data = await res.json();
+  console.log(data);
+  bindData(data.articles);
+}
+
+
+function bindData(articles){
+  const cardsContainer = document.getElementById('cards-container');
+  const newsCardTemplate = document.getElementById('template-news-card');
+
+  cardsContainer.innerHTML = '';
+
+  articles.forEach(article => {
+    if(!article.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, article);
+    cardsContainer.appendChild(cardClone);
+  });
+
+}
+
+function fillDataInCard(cardClone, article) {
+  const newsImg = cardClone.querySelector('#news-img');
+  const newsTitle = cardClone.querySelector('#news-title');
+  const newsSource = cardClone.querySelector('#news-source');
+  const newsDesc = cardClone.querySelector('#news-desc');
+
+  newsImg.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta"
+  });
+
+  newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+  cardClone.firstElementChild.addEventListener("click", () => {
+      window.open(article.url, "_blank");
+  });
+}
